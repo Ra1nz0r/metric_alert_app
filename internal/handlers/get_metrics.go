1565b460056c1ab10cf2ad443969d3d2e3828dcd
Ra1nz0r <n52rus@gmail.com>
2024-07-01 17:39:28 +0300

    Inc 1 view.

---
+++ internal/handlers/get_metrics.go
@@ -0,0 +1,59 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/ra1nz0r/metric_alert_app/internal/storage"
+)
+
+type UpdateMetricsResult struct {
+	val interface{}
+	err error
+}
+
+func UpdateMetrics(w http.ResponseWriter, r *http.Request, h storage.MetricService) {
+	codeStatus := http.StatusOK
+
+	mType := chi.URLParam(r, "type")
+	mName := chi.URLParam(r, "name")
+	mValue := chi.URLParam(r, "value")
+
+	var umr UpdateMetricsResult
+
+	switch {
+	case strings.TrimSpace(mName) == "":
+		codeStatus = http.StatusNotFound
+	case mType == "gauge" && codeStatus != 404:
+		umr.val, umr.err = h.UpdateGauge(mName, mValue)
+	case mType == "counter" && codeStatus != 404:
+		umr.val, umr.err = h.UpdateCounter(mName, mValue)
+	default:
+		codeStatus = http.StatusBadRequest
+	}
+
+	if umr.err != nil {
+		ErrReturn(umr.err, w)
+		return
+	}
+
+	res, errJSON := json.Marshal(umr.val)
+	if errJSON != nil {
+		http.Error(w, errJSON.Error(), http.StatusInternalServerError)
+		//logerr.ErrEvent("failed attempt json-marshal response", errJSON)
+		//w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+
+	w.WriteHeader(codeStatus)
+
+	if _, errWrite := w.Write([]byte(res)); errWrite != nil {
+		log.Print("failed attempt WRITE response")
+		return
+	}
+}
