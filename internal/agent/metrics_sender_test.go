bae0057a992801443043e7ef7ca3c5a82f543858
Ra1nz0r <n52rus@gmail.com>
2024-08-04 14:08:05 +0300

    Fix.

--- internal/agent/metrics_sender_test.go
+++ internal/agent/metrics_sender_test.go
@@ -1,10 +1,6 @@
 package agent
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -25,42 +21,4 @@ func UpdateMetrics(t *testing.T) {
 
 		ss.UpdateMetrics()
 	})
-}
-
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World")
-}
-
-func TestMakeRequest(t *testing.T) {
-	type args struct {
-		resURL string
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			testServer := httptest.NewServer(http.HandlerFunc(HelloWorld))
-			defer testServer.Close()
-			testClient := testServer.Client()
-
-			resp, err := testClient.Get(testServer.URL)
-			if err != nil {
-				t.Errorf("Get error: %v", err)
-			}
-			if resp.StatusCode != http.StatusOK {
-				t.Errorf("response code is not 200: %d", resp.StatusCode)
-			}
-			data, err := io.ReadAll(resp.Body)
-			if err != nil {
-				t.Errorf("io.ReadAll error: %v", err)
-			}
-			if string(data) != "Hello World\n" {
-				t.Error("response body does not equal to Hello World")
-			}
-		})
-	}
 }
